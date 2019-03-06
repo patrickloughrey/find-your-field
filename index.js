2477cb18e5b084de0c6208c8f10c6bb64cddd83e
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2019-03-05 21:32:41 -0500

    Added Google OAuth capability so user can sign in with Gmail

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({Hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('Access token:', accessToken);
+      console.log('Refresh token:', refreshToken);
+      console.log('Profile:', profile);
+
+    }
+  )
+);
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 /* Heroku environment variables */
 const PORT = process.env.PORT || 5000;
