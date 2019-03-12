d56e6ed11b9217bbab63b9f0f5a9f8d8fa64583c
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2019-03-12 16:30:22 -0400

    Added mongo URI to connect to Mongo cluster

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const passportConfig = require('./services/passport');
 const authRoutes = require('./routes/authRoutes')
 
 const app = express();
 
+/* Pass Express app to Passport routes */
 authRoutes(app);
+
+/* Connect to MongoDB Cluster */
+mongoose.connect(keys.mongoURI);
 
 /* Heroku environment variables */
 const PORT = process.env.PORT || 5000;
