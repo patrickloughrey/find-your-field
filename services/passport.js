a497fc6379315c51d2697be19b1e40df52bde6ec
Patrick Loughrey <patrickloughrey@Patricks-MBP.home>
2019-03-25 00:36:48 -0400

    Added logic that prevents duplicate users being added to the database

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,16 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleID: profile.id }).save();
+        User.findOne({ googleID: profile.id}).then(existingUser => {
+              if (existingUser) {
+                  /* User already exists */
+                  done(null, existingUser);
+              } else {
+                  /* Make a new record for new User */
+                  new User({ googleID: profile.id }).save()
+                    .then(user => done(null, user));
+              }
+          })
     }
   )
 );
