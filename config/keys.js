1d2e0781dfcf832438b0528481c53bdf2fd0fecb
Patrick Loughrey <ploughrey93@gmail.com>
2019-03-28 00:07:01 -0400

    Added and edited Mongo, Google, & Heroku credentials for dev & prod environments

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+/* Figure out Production or Development Environment */
+if (process.env.NODE_ENV == 'production') {
+	module.exports = require('./prod');
+} else {
+	module.exports = require('./dev');
+}
