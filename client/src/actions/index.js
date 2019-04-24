189706dfc5765eb9392d794679e2da720fd7645b
Patrick Loughrey <ploughrey93@gmail.com>
2019-04-23 21:06:07 -0400

    Built action creators for dispatching actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => async (dispatch) => {
+	const res = await axios.get('/api/currentUser');
+
+	dispatch({ type: FETCH_USER, payload: res.data })
+
+};
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
+// import Landing from './Landing';
 
 const Landing = () => <h2> </h2>;
 
@@ -10,18 +13,25 @@ const Dashboard = () => <h2> Dashboard </h2>;
 const NewHost = () => <h2> New Host </h2>;
 
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact path="/" component={Landing} />
-					<Route path="/dashboard" component={Dashboard} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route path="/dashboard" component={Dashboard} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+
+	}
 };
 
-export default App;
+export default connect(null, actions)(App);
